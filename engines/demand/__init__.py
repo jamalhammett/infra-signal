d3587740753581de618c3245ad4fc879e84baefd
jamalhammett <jamalhammett@gmail.com>
2026-07-13 23:29:42 -0400

    Update __init__.py

--- engines/demand/__init__.py
+++ engines/demand/__init__.py
@@ -1,1 +1,11 @@
+"""Allen Hammett Demand Intelligence package."""
 
+from engines.demand.demand_engine import (
+    DemandEngine,
+    DemandIntelligence,
+)
+
+__all__ = [
+    "DemandEngine",
+    "DemandIntelligence",
+]
